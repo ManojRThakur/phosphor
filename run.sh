796622fcb7700d55b7ff40a0aafcc73cd88de7c1
Joe Cox <joeyalancox@gmail.com>
2015-03-16 15:11:35 -0700

    Added run & setup scripts

---
+++ run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "$0: provide a benchmark"
+    exit 1
+fi
+
+bm=$1
+
+ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+PHOSPHOR_HOME=$ROOT
+PETABLOX_HOME=$( cd $ROOT && cd .. && pwd )/petablox
+DOOP_HOME=$PETABLOX_HOME/doop-r160113-bin
+JRE_INST_HOME=$PHOSPHOR_HOME/jre-inst
+
+# Step 1: Generate methods_inst using doop and cp to $PHOSPHOR_HOME
+cd $PETABLOX_HOME
+vagrant up
+vagrant ssh -c 'cd /vagrant/doop-r160113-bin && ./exec-dacapo.sh $bm > logs/$bm.log'
+cp $DOOP_HOME/ucla-pls/methods_inst $PHOSPHOR_HOME/methods
+
+# Step 2: Run phosphor_pi.jar
+rm -r $PHOSPHOR_HOME/$bm-inst > /dev/null
+cd $PHOSPHOR_HOME
+./instrument.sh $bm
+
+cd $PHOSPHOR_HOME/$bm-inst
+# ./run-instrumented.sh $
+$JRE_INST_HOME/bin/java -Xdebug -Xbootclasspath/a:$PHOSPHOR_HOME/phosphor_pi.jar -javaagent:$PHOSPHOR_HOME/phosphor_pi.jar Harness $bm
---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+if [ -z "$JAVA_HOME" ]; then
+    echo "ERR: JAVA_HOME must be set"
+fi
+
+if [ -z "$1" ]; then
+    echo "$0: please provide dacapo-2009.tar.gz location"
+fi
+
+$DACAPO=$1
+
+ROOT=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+PHOSPHOR_HOME=$ROOT
+PETABLOX_HOME=$( cd $ROOT && cd .. && pwd )/petablox
+DOOP_HOME=$PETABLOX_HOME/doop-r160113-bin
+JRE_INST_HOME=$PHOSPHOR_HOME/jre-inst
+
+# Step 1: Setup dacapo jars in phosphor folder && doop folder
+cd $ROOT
+$scratch=$(mktemp -d XXXXXXXX)
+cp $DACAPO $scratch
+cd $scratch
+
+tar -xzf dacapo-2009.tar.gz
+rm dacapo-2009/.DS_Store > /dev/null
+
+cp -R dacapo-2009/ $DOOP_HOME
+
+for bm in dacapo-2009/*; do
+    rm -r $PHOSPHOR_HOME/$bm > /dev/null
+    mkdir -p $PHOSPHOR_HOME/$bm
+    cp $DOOP_HOME/dacapo-2009/$bm/$bm.jar $PHOSPHOR_HOME/$bm/
+    cd $PHOSPHOR_HOME/$bm; jar xf $bm.jar
+done
+
+cd $ROOT
+rm -rf $scratch
+
+# Step 2: Provision VM correctly
+vagrant up
+vagrant ssh -c 'sudo apt-get update && sudo apt-get upgrade'
+vagrant provision
+
+# Step 3: Instrument JVM
+cd $PHOSPHOR_HOME
+java -jar phosphor.jar $JAVA_HOME/jre $JRE_INST_HOME
